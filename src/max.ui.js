e033c611df3ae922e3c08c3eb79daca80f46408c
alberto.duran <alberto.duran@upcnet.es>
2022-07-28 12:44:36 +0200

    Añadir funciones de frontend en tags <script> para templates y templatesactivities, falta templateschat

--- src/max.ui.js
+++ src/max.ui.js
@@ -11,7 +11,7 @@
     jq.fn.maxUI = function(options) {
         // Keep a reference of the context object
         var maxui = this;
-        maxui.version = '5.0.29.9';
+        maxui.version = '5.0.29.10';
         maxui.templates = max.templates();
         maxui.utils = max.utils();
         var defaults = {
