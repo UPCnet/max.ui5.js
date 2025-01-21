a30d095efbead6e0573b04320ec1553043a9b883
Pilar Marinas <pilar.marinas@upcnet.es>
2025-01-21 11:00:06 +0100

    [PY3] format

--- setup_maxui.py
+++ setup_maxui.py
@@ -5,7 +5,6 @@ import os
 import sys
 import json
 
-
 ORIGINAL_MAXUI_FONT_URL = 'font'
 DEFAULT_MAXUI_FONTS_URL = '/maxui/font'
 DEFAULT_MAXUI_FONTS_FOLDER = './maxui/font'
@@ -20,7 +19,8 @@ def saveConfiguration(config):
         Loads stored configuration from .maxui_setup
     """
     dump = json.dumps(config, indent=4, sort_keys=True)
-    open('.maxui_setup', 'w').write(dump)
+    with open('.maxui_setup', 'w') as f:
+        f.write(dump)
 
 
 def getConfiguration():
@@ -33,7 +33,8 @@ def getConfiguration():
             'branch': DEFAULT_MAXUI_BRANCH,
         }
         saveConfiguration(default)
-    data = open('.maxui_setup').read()
+    with open('.maxui_setup', 'r') as f:
+        data = f.read()
     config = json.loads(data)
     return config
 
@@ -46,8 +47,8 @@ def downloadFile(config, filename, raw=True):
     params = dict(config)
     params['filename'] = filename
     params['tree'] = 'raw' if raw else 'tree'
-    url = '{github_url}/{tree}/{branch}/{filename}'.format(**params)
-    sys.stdout.write(" Downloading {} ".format(url))
+    url = f"{config['github_url']}/{params['tree']}/{config['branch']}/{filename}"
+    sys.stdout.write(f" Downloading {url} ")
     sys.stdout.flush()
     response = requests.get(url, verify=False)
     if response.status_code != 200:
@@ -58,85 +59,79 @@ def downloadFile(config, filename, raw=True):
 
 
 def main():
-    # Setup configuration parameters
-    # User will be asked if not setted
-
     print()
     config = getConfiguration()
 
     if 'fonts_url' not in config:
-        fonts_url = input("Fonts base_url ['{}']: ".format(DEFAULT_MAXUI_FONTS_URL))
-        fonts_url = fonts_url.strip()
-        fonts_url = fonts_url.rstrip('/')
+        fonts_url = input(
+            f"Fonts base_url ['{DEFAULT_MAXUI_FONTS_URL}']: ").strip().rstrip('/')
         config['fonts_url'] = fonts_url if fonts_url else DEFAULT_MAXUI_FONTS_URL
 
     if 'fonts_location' not in config:
-        fonts_url = input("Font files location ['{}']: ".format(DEFAULT_MAXUI_FONTS_FOLDER))
-        fonts_url = fonts_url.strip()
-        fonts_url = fonts_url.rstrip('/')
-        config['fonts_location'] = fonts_url if fonts_url else DEFAULT_MAXUI_FONTS_FOLDER
+        fonts_location = input(
+            f"Font files location ['{DEFAULT_MAXUI_FONTS_FOLDER}']: ").strip().rstrip(
+            '/')
+        config['fonts_location'] = fonts_location if fonts_location else DEFAULT_MAXUI_FONTS_FOLDER
 
     if 'js_location' not in config:
-        js_location = input("Javascript file location ['{}']: ".format(DEFAULT_MAXUI_JS))
-        js_location = js_location.strip()
+        js_location = input(
+            f"Javascript file location ['{DEFAULT_MAXUI_JS}']: ").strip()
         config['js_location'] = js_location if js_location else DEFAULT_MAXUI_JS
 
     if 'css_location' not in config:
-        css_location = input("Stylesheet file location ['{}']: ".format(DEFAULT_MAXUI_CSS))
-        css_location = css_location.strip()
+        css_location = input(
+            f"Stylesheet file location ['{DEFAULT_MAXUI_CSS}']: ").strip()
         config['css_location'] = css_location if css_location else DEFAULT_MAXUI_CSS
 
     saveConfiguration(config)
 
-    version = json.loads(downloadFile(config, 'package.json'))['version']
+    version = json.loads(downloadFile(
+        config, 'package.json').decode('utf-8'))['version']
 
-    js = downloadFile(config, 'builds/{}/maxui.min.js'.format(version))
+    js = downloadFile(config, f'builds/{version}/maxui.min.js')
     if not js:
-        print(' MAX UI Version {} build not found'.format(version))
+        print(f' MAX UI Version {version} build not found')
         sys.exit(1)
-    # Store downloaded js
-    open(config['js_location'], 'w').write(js)
+    with open(config['js_location'], 'wb') as f:
+        f.write(js)
 
-    js = downloadFile(config, 'builds/{}/maxui.min.js.map'.format(version))
-    if not js:
-        print(' MAX UI jsmap Version {} build not found'.format(version))
+    js_map = downloadFile(config, f'builds/{version}/maxui.min.js.map')
+    if not js_map:
+        print(f' MAX UI jsmap Version {version} build not found')
         sys.exit(1)
-    # Store downloaded js map
-    fname = config['js_location'].split('.js')[0]
-    open('{}.map'.format(fname), 'w').write(js)
+    fname = config['js_location'].rsplit('.js', 1)[0]
+    with open(f'{fname}.map', 'wb') as f:
+        f.write(js_map)
 
-    js = downloadFile(config, 'builds/{}/maxui.js'.format(version))
-    if not js:
-        print(' MAX UI js source Version {} build not found'.format(version))
+    js_source = downloadFile(config, f'builds/{version}/maxui.js')
+    if not js_source:
+        print(f' MAX UI js source Version {version} build not found')
         sys.exit(1)
-    # Store downloaded js source for map
     path = '/'.join(config['js_location'].split('/')[:-1])
-    open(path + '/maxui.js', 'w').write(js)
+    with open(f'{path}/maxui.js', 'wb') as f:
+        f.write(js_source)
 
-    #Download and modify CSS
-    css = downloadFile(config, 'builds/{}/maxui.min.css'.format(version))
+    css = downloadFile(config, f'builds/{version}/maxui.min.css').decode('utf-8')
     sys.stdout.write(" Modifying font links ")
     sys.stdout.flush()
-    # css = re.sub(r"(url\(['\"]?){}(['\"]?)".format(ORIGINAL_MAXUI_IMAGES_URL), r"\1{images_url}\2".format(**config), css)
-    css = re.sub(r"(url\(['\"]?){}(/maxicons['\"]?)".format(ORIGINAL_MAXUI_FONT_URL), r"\1{fonts_url}\2".format(**config), css)
-    open(config['css_location'], 'w').write(css)
+    css = re.sub(
+        rf"(url\(['\"]?){ORIGINAL_MAXUI_FONT_URL}(/maxicons['\"]?)",
+        rf"\1{config['fonts_url']}\2",
+        css
+    )
+    with open(config['css_location'], 'w') as f:
+        f.write(css)
     sys.stdout.write("✓\n")
     sys.stdout.flush()
 
-    #Download fonts
     extensions = ['eot', 'svg', 'ttf', 'woff']
     for extension in extensions:
-        fontbytes = downloadFile(config, 'builds/{}/font/maxicons.'.format(version) + extension)
-        open(config['fonts_location'] + '/maxicons.' + extension, 'w').write(fontbytes)
+        fontbytes = downloadFile(config, f'builds/{version}/font/maxicons.{extension}')
+        with open(f"{config['fonts_location']}/maxicons.{extension}", 'wb') as f:
+            f.write(fontbytes)
 
-    # #Download images
-    # images = downloadFile(config, 'img', raw=False)
-    # image_urls = re.findall(r'href=".*?/%s/img/(.*?)"' % (config['branch']), images)
-    # for image in image_urls:
-    #     imagebytes = downloadFile(config, 'img/' + image)
-    #     open(config['images_location'] + '/' + unquote(image), 'w').write(imagebytes)
+    print(f'\n MAX UI {version} setup finished\n')
 
-    print('\n MAX UI {} setup finished\n'.format(version))
 
 if __name__ == "__main__":
     main()
